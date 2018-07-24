d3200d0bea5fe61cdfef2166bab0f4fef4ca47d8
matheusfcunha <matheusfbcunha@gmail.com>
2018-07-24 14:49:10 -0300

    Completes more exercises

---
+++ exercise_4.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise_4) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; exercise 4 http://www.htdp.org/2018-01-06/Book/part_one.html#%28counter._%28exercise._arith-s2%29%29
+
+(define str "helloworld")
+(define i 5)
+
+(string-append
+ (substring str 0 (- i 1)) (substring str (+ i 1) (string-length str)))
